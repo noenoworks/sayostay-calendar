a7eed97ba4926427aa261f593dfd7755b57a273c
noenoworks <noenoworks@gmail.com>
2026-08-29 14:18:32 +0900

    Spring Bootプロジェクトの初期構成

---
+++ src/main/java/com/noenoworks/sayostaycalendar/SayostayCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.noenoworks.sayostaycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayostayCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayostayCalendarApplication.class, args);
+	}
+
+}
