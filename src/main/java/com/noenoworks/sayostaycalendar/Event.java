5b046ebae67b45a0b1c3aa0bdd7665cba73f58c2
noenoworks <noenoworks@gmail.com>
2026-09-01 21:15:35 +0900

    イベントに備考欄を追加

--- src/main/java/com/noenoworks/sayostaycalendar/Event.java
+++ src/main/java/com/noenoworks/sayostaycalendar/Event.java
@@ -25,26 +25,39 @@ public class Event {
     private String venue;
     // チケット購入フラグ
     private boolean ticketPurchased;
+    // 備考
+    private String note;
 
     // getter / setter
+    // id
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
+    // イベント名
     public String getEventName() { return eventName; }
     public void setEventName(String eventName) { this.eventName = eventName; }
 
+    // 日付
     public LocalDate getEventDate() { return eventDate; }
     public void setEventDate(LocalDate eventDate) { this.eventDate = eventDate; }
 
+    // 開場時間
     public LocalTime getOpenTime() { return openTime; }
     public void setOpenTime(LocalTime openTime) { this.openTime = openTime; }
 
+    // 開演時間
     public LocalTime getStartTime() { return startTime; }
     public void setStartTime(LocalTime startTime) { this.startTime = startTime; }
 
+    // 会場
     public String getVenue() { return venue; }
     public void setVenue(String venue) { this.venue = venue; }
 
+    // チケット購入フラグ
     public boolean isTicketPurchased() { return ticketPurchased; }
     public void setTicketPurchased(boolean ticketPurchased) { this.ticketPurchased = ticketPurchased; }
+
+    // 備考
+    public String getNote() { return note; }
+    public void setNote(String note) { this.note = note; }
 }
